b35001de49aa95e44cb69c410b3461be2de9ffca
rishi <rishijash@hotmail.com>
2024-03-31 17:02:11 -0700

    Fix GeminiGetCleanedJsonResponse

--- internal/utils/geminiUtil.go
+++ internal/utils/geminiUtil.go
@@ -11,8 +11,10 @@ func GeminiGetCleanedJsonResponse(geminiResponse string) string {
 	cleanedResponse = strings.ReplaceAll(cleanedResponse, "\n", "")
 
 	// Also clean trailing commas
-	trailingCommaPattern := regexp.MustCompile(`,(?=\s*[}\]])`)
-	cleanedResponse = trailingCommaPattern.ReplaceAllString(cleanedResponse, "")
+	// Adjusted pattern to match a comma followed by any number of whitespace characters
+	// and then a closing bracket or brace, without using a lookahead.
+	trailingCommaPattern := regexp.MustCompile(`,\s*([}\]])`)
+	cleanedResponse = trailingCommaPattern.ReplaceAllString(cleanedResponse, "$1")
 
 	return cleanedResponse
 }
